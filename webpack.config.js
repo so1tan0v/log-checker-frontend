3e59c42975380d380b7e711593f552b1eb3e35fd
Soltanov Aleksandr <sashasyltanov@gmail.com>
2023-08-06 00:51:01 +0400

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
                     options: {
                         compilerOptions: {
                             noEmit: false
-                        },
+                        }
                     }
                 }
             },
